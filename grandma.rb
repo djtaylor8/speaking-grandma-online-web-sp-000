027aa944814105feee86a3b4c15f08f061c12b20
DJ Taylor <dtaylor88@gmail.com>
2021-05-30 05:06:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
      "HUH?! SPEAK UP, SONNY!"
-   else 
+   else
     "NO, NOT SINCE 1938!"
   end
   if phrase = "I LOVE YOU GRANDMA!"
@@ -14,7 +14,7 @@ end
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
